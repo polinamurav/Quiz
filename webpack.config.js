bc5c37b8bbb4cfc3cb281bec8ae4ac7387723a18
Polina Muravieva <polinamur3@gmail.com>
2024-08-08 23:17:53 +0300

    used DevServer from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    devServer: {
+        static: '.dist',
+        compress: true,
+        port: 9000,
+    },
     plugins: [
         new HtmlWebpackPlugin({
         template: "./index.html"
