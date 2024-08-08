460b519118b8f165232fc3ac5b4ccb5b2508c9e6
Polina Muravieva <polinamur3@gmail.com>
2024-08-08 23:13:39 +0300

    used transpiling babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: './src/app.js',
+    mode: "development",
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
@@ -22,4 +23,20 @@ module.exports = {
             ],
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
 };
