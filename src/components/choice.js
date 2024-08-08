a6e2c09f86bdf64bad07588bae127f2bee456d1a
Polina Muravieva <polinamur3@gmail.com>
2024-08-08 22:32:26 +0300

    webpack connection

--- src/components/choice.js
+++ src/components/choice.js
@@ -43,7 +43,7 @@ export class Choice {
                 choiceOptionArrowElement.className = 'choice-option-arrow';
 
                 const choiceOptionImageElement = document.createElement('img');
-                choiceOptionImageElement.setAttribute('src', 'static/images/arrow.png');
+                choiceOptionImageElement.setAttribute('src', '/images/arrow.png');
                 choiceOptionImageElement.setAttribute('alt', 'Стрелка');
 
                 choiceOptionArrowElement.appendChild(choiceOptionImageElement);
