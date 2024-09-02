f9770f0d8803aa5081e2611067da8222abb57b47
Polina Muravieva <polinamur3@gmail.com>
2024-09-02 23:19:21 +0300

    changed arrow

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Как запускать:
-в папке backend запускаем команду npm start
+инициализируй еще все пакеты: npm i в каждой папке
+
+в папке backend запускаем команду npm start.
+
 в папке frontend (cd frontend) запускаем команду npm run dev
--- frontend/src/components/result.js
+++ frontend/src/components/result.js
@@ -1,7 +1,7 @@
 import {UrlManajer} from "../utils/url-manajer.js";
-import {CustomHttp} from "../services/custom-http";
-import config from "../../config/config";
-import {Auth} from "../services/auth";
+import {CustomHttp} from "../services/custom-http.js";
+import config from "../../config/config.js";
+import {Auth} from "../services/auth.js";
 
 export class Result {
     constructor() {
